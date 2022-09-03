9bfee49c129504b9162b7349bc2c6c3075623e2f
unknown <mdishabls@gmail.com>
2022-09-03 23:35:00 +0530

    sql-2

--- sql2.sql
+++ sql2.sql
@@ -5,5 +5,18 @@
 -- author varchar(30),
 -- theme varchar(30)
 -- );
-select title,author from books
-where theme like "%y" and genre not like "%r" order by theme desc;
+
+-- insert into books values('horror','The haunting','Steve K','forest horor');
+-- insert into books values('comedy','Echoing Laughters','Charles G','friends comedy');
+-- insert into books values('comedy','Happiens','Carl','parody');
+-- insert into books values('lifestyle','CookBook','Julien S','Recipes');
+-- insert into books values('lifestyle','CookBook','Julien S','Recipes');
+-- insert into books values('thriller','The Cause','Julien S','Treasure Mystery');
+-- select * from books;
+
+-- select title,author from books
+-- where theme like "%y" and genre not like "%r" order by theme desc;
+
+-- create view themeview as
+-- select theme from books where author like "% %";
+select count(*) from themeview;
